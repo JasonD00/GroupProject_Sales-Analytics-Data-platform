6e33bfdf9fd47361554877404a5da3ae420dd5cc
Josh <joshuaboyne021@gmail.com>
2026-06-25 13:43:49 +0100

    created login system branch

--- src/main/java/com/salesplatform/sales_analytics_api/entity/User.java
+++ src/main/java/com/salesplatform/sales_analytics_api/entity/User.java
@@ -1,4 +1,34 @@
 package com.salesplatform.sales_analytics_api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users", schema = "dbo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "tier", nullable = false)
+    private String tier;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
