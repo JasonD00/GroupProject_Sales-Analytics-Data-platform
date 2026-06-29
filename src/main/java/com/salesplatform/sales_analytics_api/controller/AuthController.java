ac4026033a5dca798e27a08194e01f634c2d27f6
Josh <joshuaboyne021@gmail.com>
2026-06-29 17:43:43 +0100

    updated register endpoint

--- src/main/java/com/salesplatform/sales_analytics_api/controller/AuthController.java
+++ src/main/java/com/salesplatform/sales_analytics_api/controller/AuthController.java
@@ -2,18 +2,22 @@ package com.salesplatform.sales_analytics_api.controller;
 
 import com.salesplatform.sales_analytics_api.dto.LoginRequest;
 import com.salesplatform.sales_analytics_api.dto.LoginResponse;
+import com.salesplatform.sales_analytics_api.dto.RegisterRequest;
 import com.salesplatform.sales_analytics_api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
-    Entry point for auth HTTP request
-    POST /api/auth/login
-    
-    Main body: {"username": "jason", "password": "password"}
-    Returns: {"username": "jason", "tier": "ENTERPRISE"}
-*/
+       Auth Controller
+
+       entry point for all auth requests: (AuthService)
+
+       Creat a new user and return JWT token on success
+ */
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,17 +25,15 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
-    // POST /api/auth/login
+    // api/auth/register
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
+    // /api/auth/login
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-        try {
-            System.out.println("=== CONTROLLER DEBUG ===");
-            LoginResponse response = authService.login(request);
-            System.out.println("Login successful, returning response: " + response);
-            return ResponseEntity.ok(response);
-        } catch (RuntimeException e) {
-            System.out.println("Login failed with error: " + e.getMessage());
-            return ResponseEntity.status(401).body("Invalid username or password");
-        }
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
     }
 }
--- src/main/java/com/salesplatform/sales_analytics_api/service/AuthService.java
+++ src/main/java/com/salesplatform/sales_analytics_api/service/AuthService.java
@@ -3,55 +3,68 @@ package com.salesplatform.sales_analytics_api.service;
 import com.salesplatform.sales_analytics_api.config.JwtConfig;
 import com.salesplatform.sales_analytics_api.dto.LoginRequest;
 import com.salesplatform.sales_analytics_api.dto.LoginResponse;
+import com.salesplatform.sales_analytics_api.dto.RegisterRequest;
 import com.salesplatform.sales_analytics_api.entity.User;
 import com.salesplatform.sales_analytics_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-/*
-    Handles all the login auth logic
-
-    - receives the username + password from controller
-    - looks up user in dbo.users by username
-    - uses BCrypt to verify the password against the stored hashed password
-    - returns LoginResponse with username + tier if valid
-    - throws RuntimeException if username isnt found or if the password is wrong
-
-    BCryptPasswordEncoder.matches() compares the plain text password with the hash without storing the plain text
-*/
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
     private final JwtConfig jwtConfig;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-    public LoginResponse login(LoginRequest request) {
+    // Register ne user - pword hashed with bcrypt before saving
+    public String register(RegisterRequest registerRequest) {
 
-        System.out.println("=== LOGIN DEBUG ===");
-        System.out.println("Username received: " + request.getUsername());
-        System.out.println("Password received: " + request.getPassword());
-
-        User user = userRepository.findByUsername(request.getUsername())
-                .orElseThrow(() -> new RuntimeException("Invalid username or password"));
-
-        System.out.println("User found: " + user.getUsername());
-        System.out.println("Hash in DB: " + user.getPassword());
-        System.out.println("Password matches: " + passwordEncoder.matches(request.getPassword(), user.getPassword()));
-
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            throw new RuntimeException("Invalid username or password");
+        // make sure username isnt taken already
+        if (userRepository.findByUsername(registerRequest.getUsername()).isPresent()) {
+            throw new RuntimeException("Username is already in use" + registerRequest.getUsername());
         }
 
+        User user = User.builder()
+                .username(registerRequest.getUsername())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .tier(registerRequest.getTier())
+                .createdAt(LocalDate.now())
+                .build();
+
+        userRepository.save(user);
+        return "User registered successfully";
+
+    }
+
+    // For loggingnin a existing user
+    // return a JWT token with the username and tier
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        // find the user or throw exception
+        User user = userRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() -> new RuntimeException("Username not found"));
+
+        // cjeck the password matches
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Passwords don't match");
+        }
+
+        // generate JWT token with username and tier
         String token = jwtConfig.generateToken(user.getUsername(), user.getTier());
 
         return LoginResponse.builder()
-                .username(user.getUsername())
-                .tier(user.getTier())
                 .token(token)
+                .tier(user.getTier())
                 .build();
     }
+
+
+
 }
